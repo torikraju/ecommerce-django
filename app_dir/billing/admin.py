0161db96ff01dd44aa63cbc6d3e571f13dbf6b54
torikraju <torikraju@gmail.com>
2019-04-25 19:09:54 +0600

    Charge the Customer

--- app_dir/billing/admin.py
+++ app_dir/billing/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from app_dir.billing.models import BillingProfile, Card
+from app_dir.billing.models import BillingProfile, Card,Charge
 
 admin.site.register(BillingProfile)
 admin.site.register(Card)
+admin.site.register(Charge)
