f0ca02589234f5c6aa6287954d33d718a9a961a9
torikraju <torikraju@gmail.com>
2019-04-27 14:37:07 +0600

    [Mailchimp Integration] - Django & Mailchimp

--- app_dir/marketing/admin.py
+++ app_dir/marketing/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MarketingPreference
+
+
+class MarketingPreferenceAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'subscribed', 'updated_at']
+    readonly_fields = ['mailchimp_msg', 'mailchimp_subscribed', 'created_at', 'updated_at']
+
+    class Meta:
+        model = MarketingPreference
+        fields = [
+            'user',
+            'subscribed',
+            'mailchimp_msg',
+            'mailchimp_subscribed',
+            'created_at',
+            'updated_at'
+        ]
+
+
+admin.site.register(MarketingPreference, MarketingPreferenceAdmin)
--- app_dir/marketing/models.py
+++ app_dir/marketing/models.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+
+from app_dir.marketing.utils import Mailchimp
 
 User = settings.AUTH_USER_MODEL
 
@@ -9,19 +11,40 @@ User = settings.AUTH_USER_MODEL
 class MarketingPreference(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     subscribed = models.BooleanField(default=True)
+    mailchimp_subscribed = models.NullBooleanField(blank=True)
     mailchimp_msg = models.TextField(null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.email
 
 
 @receiver(post_save, sender=MarketingPreference)
-def marketing_pref_update_receiver(sender, instance, created, *args, **kwargs):
+def marketing_pref_create_receiver(sender, instance, created, *args, **kwargs):
     if created:
-        pass
-        print("Add user to mailchimp")
+        status_code, response_data = Mailchimp().subscribe(instance.user.email)
+        print(status_code, response_data)
+
+
+@receiver(pre_save, sender=MarketingPreference)
+def marketing_pref_update_receiver(sender, instance, *args, **kwargs):
+    if instance.subscribed != instance.mailchimp_subscribed:
+        if instance.subscribed:
+            # subscribing user
+            status_code, response_data = Mailchimp().subscribe(instance.user.email)
+        else:
+            # unsubscribe user
+            status_code, response_data = Mailchimp().unsubscribe(instance.user.email)
+
+        if response_data['status'] == 'subscribed':
+            instance.subscribed = True
+            instance.mailchimp_subscribed = True
+            instance.mailchimp_msg = response_data
+        else:
+            instance.subscribed = False
+            instance.mailchimp_subscribed = False
+            instance.mailchimp_msg = response_data
 
 
 @receiver(post_save, sender=User)
